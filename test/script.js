09a085a5ede4dcc67837f4d6bc940e661d340c02
nrb2002 <tshibasubaron@yahoo.fr>
2023-09-23 23:59:57 +0100

    New updates

---
+++ test/script.js
@@ -0,0 +1,13 @@
+$('.toggle').hover(function () {
+    "use strict";
+    $('nav ul').slideToggle();
+});
+
+
+
+$(window).resize(function () {
+    "use strict";
+    if ($(window).width() > 780) {
+        $('nav ul').removeAttr('style');
+    }
+});
