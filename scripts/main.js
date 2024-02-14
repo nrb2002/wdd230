dd450fa8658d9b498185453a0a1c873ecff8e504
nrb2002 <tshibasubaron@yahoo.fr>
2024-02-14 17:36:00 +0100

    New update
    Add responsive images activity

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//Hamburger menu toggler
 const hamButton = document.querySelector('#menuToggler');
 const navigation = document.querySelector('nav');
 
@@ -5,3 +6,21 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+//Sticky navbar on scroll
+window.onscroll = function() {myFunction()};
+            
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+	if (window.scrollY >= sticky) {
+	navbar.classList.add("sticky")
+	} else {
+	navbar.classList.remove("sticky");
+	}
+}
+
+
+
