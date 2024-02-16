d95aea1f21835fb8f22c290e628427ca41426baf
nrb2002 <tshibasubaron@yahoo.fr>
2024-02-16 22:50:55 +0100

    new update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -3,16 +3,31 @@ const close = document.querySelector('#closeMenu');
 const menu = document.querySelector('#primaryMenu');
 
 
-//Open toggler
+//Open menu toggler
 open.addEventListener('click', () => {	
 	menu.classList.toggle("show");
 	open.classList.toggle("hide");
 	close.classList.toggle("show");
 });
 
-//Close toggler
+//Close menu toggler
 close.addEventListener('click', () => {	
 	menu.classList.toggle("show");
 	open.classList.toggle("hide");
 	close.classList.toggle("show");
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
