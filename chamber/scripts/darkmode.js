8aecd91dc248b942f954cdc1c967a525a2fb4988
nrb2002 <tshibasubaron@yahoo.fr>
2024-02-26 22:29:57 +0100

    New update
    Fix darkmode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,10 +1,11 @@
-var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
+let checkbox = document.getElementById("modeCheckBox"); //get the checkbox to a variable
+let label = document.getElementById("modeLabel"); //get the switch label
 
 //check storage if dark mode was on or off
 if (sessionStorage.getItem("mode") == "dark") {
   darkmode(); //if dark mode was on, run this funtion
 } else {
-  nodark(); //else run this funtion
+  light(); //else run this funtion
 }
 
 //if the checkbox state is changed, run a funtion
@@ -13,7 +14,7 @@ checkbox.addEventListener("change", function() {
   if (checkbox.checked) {
     darkmode(); //if the checkbox is checked, run this funtion
   } else {
-    nodark(); //else run this funtion
+    light(); //else run this funtion
   }
 });
 
@@ -21,12 +22,14 @@ checkbox.addEventListener("change", function() {
 function darkmode() {
   document.body.classList.add("dark-mode"); //add a class to the body tag
   checkbox.checked = true; //set checkbox to be checked state
+  label.textContent = "Light mode";
   sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
 }
 
 //function for checkbox when checkbox is not checked
-function nodark() {
+function light() {
   document.body.classList.remove("dark-mode"); //remove added class from body tag
   checkbox.checked = false; //set checkbox to be unchecked state
+  label.textContent = "Dark mode";
   sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
 }
