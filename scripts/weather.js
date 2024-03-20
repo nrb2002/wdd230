57e191719331b55b2d22f393aa626c0ccd605aa8
nrb2002 <tshibasubaron@yahoo.fr>
2024-03-20 23:18:29 +0100

    New update

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,105 +1,60 @@
-/* Fetching Data from OpenWeatherMap API */
-const myCity = "Kinshasa";
+//select all of the HTML elements that will need to be manipulated and assign them to const variables.
+const cityName = document.querySelector("#city");
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#description");
+const humidity = document.querySelector("#humidity");
+const windSpeed= document.querySelector("#wind-speed");
 
-let weather = {
-  apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
+//Specify the latitude and longitude of Trier, Germany using the information you have gathered and the examples provided.
+const lat = -4.32;
+const lon = 15.31;
+//Set the units to imperial: "units=imperial"
+const units = "metric";
+//Provide your API key: "appid=[enter your key here]"
+const apiKey = "4fbad2e30a8af0dbabfe5ed17d272cf8";
+//Declare a const variable named "url" and assign it a valid URL string as given in the openweathermap api documentation.
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
+//Define an asynchronous function named "apiFetch()" that uses a try block to handle errors.
+async function apiFetch(){
+    try{
+        //Store the results of the URL fetch into a variable named "response".
+        const response = await fetch(url);
+        //If the response is OK, then store the result of response.json() conversion in a variable named "data", 
+        if(response.ok){
+            const data = await response.json();
+            //Output the results to the console for testing
+            console.log(data);
+            //output to the given HTML document
+            displayResults(data);
+        //Else, throw an Error using the response.text().
+        }else{
+            throw Error(await response.text());
         }
-        return response.json();
-      })
-      .then((data) => this.displayCurrentWeather(data));
-  },
-  displayCurrentWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").textContent = `${name} Weather Info`;
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").textContent = description;
-    document.querySelector(".temp").textContent = temp + "°C";
-    document.querySelector(".imperial").textContent = `(${(temp*9/5+32).toFixed(2)} °F)`; //Convert celsius to imperial
-    document.querySelector(".humidity").textContent = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").textContent = "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-  },
-  search: function () {
-    this.fetchWeather(myCity);
-  },
-};
-
-/* Fetching Data from OpenCageData Geocoder */
-let geocode = {
-  reverseGeocode: function (latitude, longitude) {
-    let apikey = "90a096f90b3e4715b6f2e536d934c5af";
-
-    let api_url = "https://api.opencagedata.com/geocode/v1/json";
-
-    let request_url =
-      api_url +
-      "?" +
-      "key=" +
-      apikey +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-
-    let request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
-
-    request.onload = function () {
-
-      if (request.status == 200) {
-        let data = JSON.parse(request.responseText);
-        weather.fetchWeather(data.results[0].components.city);
-        console.log(data.results[0].components.city)
-      } else if (request.status <= 500) {
-
-        console.log("unable to geocode! Response code: " + request.status);
-        let data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-
-    request.onerror = function () {
-      console.log("unable to connect to server");
-    };
-
-    request.send(); 
-  },
-  getLocation: function() {
-    function success (data) {
-      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    //Finish off the catch block by outputting any try error to the console.
+    }catch (error){
+        console.log(error);
     }
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, console.error);
-    }
-    else {
-      weather.fetchWeather(myCity);
-    }
-  }
-};
+}
+//invoke the apiFetch() function with a call
+apiFetch();
 
-//Search weather information
-weather.fetchWeather(myCity);
+//Build the displayResults function to output to the given HTML document
+function displayResults(data){
+    cityName.innerHTML = "Kinshasa";
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;C`;
+    
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIcon.setAttribute("src",iconsrc);
+    weatherIcon.setAttribute("alt", "Weather Icon");
+    weatherIcon.setAttribute("loading","lazy" );
+    
+    let desc = data.weather[0].description;
+    captionDesc.textContent = `${desc}`;
 
-//Reload page every 60 min
-setInterval(function() {
-  location.reload();
-}, 3600000);
+    humidity.textContent = `${data.main.humidity}%`; 
+    windSpeed.textContent = `${data.wind.speed}m/s`;
 
-
-
+    
+}
